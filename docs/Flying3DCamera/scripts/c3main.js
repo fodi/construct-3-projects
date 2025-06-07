7f3f55e4a142bce4372d8bd5c6c887bfa79e2c65
Bence Fodor <1785582+fodi@users.noreply.github.com>
2025-06-07 19:21:02 +0200

    Added Flying3DCamera template project

---
+++ docs/Flying3DCamera/scripts/c3main.js
@@ -0,0 +1,3 @@
+import "./c3runtime.js";
+import "./objRefTable.js";
+import "./project/javaScriptInEvents.js";
---
+++ docs/Flying3DCamera/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,11 @@
+
+
+const scriptsInEvents = {
+
+	async ["Futilities🧰_Event8_Act1"](runtime, localVars)
+	{
+		runtime.setReturnValue(runtime.objects[localVars["name"]] ? 1 : 0)
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
